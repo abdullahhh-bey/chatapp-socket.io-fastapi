80568b93578f021b5097c3fdc7146485f6983f6e
Alex <kibgheeo775533@gmail.com>
2025-10-07 00:30:04 +0500

    Implemented socket with testing

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from fastapi import FastAPI
 import socketio
 
-#it is simply making a compatible websocket transport server
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 
-app = FastAPI()
-
-#it is combining the fastapi & socket.io
-app = socketio.ASGIApp(sio, other_asgi_app=app)
+fastapi_app = FastAPI()
+app = socketio.ASGIApp(sio, other_asgi_app=fastapi_app)
 
 
-@sio.on("connect")
-async def connect(id):
-    print(f"User:{id} connected")
-    await sio.emit("message" , f"User:{id} enters the chat")
-    
-    
-@sio.on("disconnect")
-async def disconnect(id):
-    print(f"User:{id} disconnected")
-    await sio.emit("message" , f"User:{id} left the chat",  skip_sid=id) #it means that everyone get the mesage of disconnection excepts the one who disconnect
+
+@sio.event
+async def connect(sid, environ):
+    print(f"User connected: {sid}")
+    await sio.emit("message", {"user": "Server", "text": f"{sid[:4]} joined the chat"})
+
+
+@sio.event
+async def disconnect(sid):
+    print(f"User disconnected: {sid}")
+    await sio.emit("message", {"user": "Server", "text": f"{sid[:4]} left the chat"})
 
 
 @sio.on("message")
-async def message(name, data):
-    print(f"{id} messsaged")
-    await sio.emit("message" , f"{name}\n{data}\n")
-    
+async def message(sid, data):
+    print(f"{data['user']}: {data['text']}")
+    await sio.emit("message", data)  
